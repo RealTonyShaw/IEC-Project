b53cdf018e3ae02e7fc604abf8e2a9a62cd68a7d
RealTonyShaw <tonyshaw123@hotmail.com>
2019-09-02 20:43:54 +0800

    Update Interpolation

--- Assets/Scripts/Network/IHermiteInterpolation.cs
+++ Assets/Scripts/Network/IHermiteInterpolation.cs
@@ -11,15 +11,5 @@ public interface IHermiteInterpolation
     /// <param name="end">第二个已知点</param>
     /// <param name="forecast">需要预测的点</param>
     /// <returns>预测点的 Vector3</returns>
-    Vector3 Hermite(Vector3 start, Vector3 end, float forecast);
-
-    /// <summary>
-    /// 三点埃尔米特插值
-    /// </summary>
-    /// <param name="start">第一个已知点</param>
-    /// <param name="mid">第二个已知点</param>
-    /// <param name="end">第三个已知点</param>
-    /// <param name="forecast">需要预测的点</param>
-    /// <returns>预测点的 Vector3</returns>
-    Vector3 Hermite(Vector3 start, Vector3 mid, Vector3 end, float forecast);
+    float Hermite(float x0, float y0, float derivative0, float x1, float y1, float derivative1, float forecastX);
 }
