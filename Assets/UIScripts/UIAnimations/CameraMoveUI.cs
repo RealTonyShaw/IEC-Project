b4f883113e53c2d00a89a68c3b6450d97d56bbc6
1170300302 <1312736592@qq.com>
2019-09-02 20:41:51 +0800

    Add UI

---
+++ Assets/UIScripts/UIAnimations/CameraMoveUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveUI : MonoBehaviour
+{
+    [SerializeField]
+    private float distance = 20.0f;
+    [SerializeField]
+    private float horizontalSpeed = 10;
+    [SerializeField]
+    private float verticalSpeed = 5;
+    [SerializeField]
+    private Transform centerTransform;
+
+    private void Update()
+    {
+        transform.position = centerTransform.position + new Vector3(Mathf.Sin(Time.time * Mathf.Deg2Rad * horizontalSpeed), Mathf.Sin(Time.time * Mathf.Deg2Rad * verticalSpeed), Mathf.Cos(Time.time * Mathf.Deg2Rad * horizontalSpeed)) * Mathf.Abs(Mathf.Sin(Time.time * 0.125f)) * distance;
+        transform.LookAt(centerTransform);
+    }
+}
---
+++ Assets/UIScripts/UIAnimations/TextAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+
+    private float scintillationInterval = 2;
+    private float alpha = 1;
+    private Color textColor = Color.white;
+    private bool forward = false;
+
+    private void Update()
+    {
+        if (forward)
+        {
+            alpha += Time.smoothDeltaTime * scintillationInterval;
+            if (alpha >= 1)
+            {
+                forward = !forward;
+            }
+        }
+        else
+        {
+            alpha -= Time.smoothDeltaTime * scintillationInterval;
+            if (alpha <= 0)
+            {
+                forward = !forward;
+            }
+        }
+        textColor.a = alpha;
+        text.color = textColor;
+    }
+}
