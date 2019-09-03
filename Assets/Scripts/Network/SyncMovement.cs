95bdd03616827184a42aff74e255baacccf75e39
RealTonyShaw <tonyshaw123@hotmail.com>
2019-09-03 20:35:24 +0800

    Update .meta

--- Assets/Scripts/Network/SyncMovement.cs
+++ Assets/Scripts/Network/SyncMovement.cs
@@ -6,16 +6,29 @@ public class SyncMovement : ISyncMovement
 {
     Unit unit;
 
+    // 保存上上次 Transform 参数
+    long firstTransformInstant;
+    Vector3 firstPosition;
+    Quaternion firstRotation;
+    Vector3 firstVelocity;
+    Vector3 firstAngularVelocity;
+
+    // 保存上次 Transform 参数
     long lastTransformInstant;
     Vector3 lastPosition;
     Quaternion lastRotation;
     Vector3 lastVelocity;
     Vector3 lastAngularVelocity;
 
+    // 保存上次 Acceleration 参数
     long lastAccelerationInstant;
     int lastAcceleration;
     int lastAngularAcceleration;
     Vector3 lastCameraForward;
+
+    // 预测的参数
+    long currentInstant;
+
 
     public void Init(Unit unit)
     {
@@ -24,7 +37,10 @@ public class SyncMovement : ISyncMovement
 
     public void SyncAcceleration(long instant, int acceleration, int angularAcceleration, Vector3 cameraForward)
     {
-        throw new System.NotImplementedException();
+        this.lastAccelerationInstant = instant;
+        this.lastAcceleration = acceleration;
+        this.lastAngularAcceleration = angularAcceleration;
+        this.lastCameraForward = cameraForward;
     }
 
     public void SyncTransform(long instant, Vector3 position, Quaternion rotation, Vector3 velocity, Vector3 angularVelocity)
@@ -38,6 +54,6 @@ public class SyncMovement : ISyncMovement
 
     public void Update(float dt)
     {
-        throw new System.NotImplementedException();
+        
     }
 }
