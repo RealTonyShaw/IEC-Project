57c41de4ae624eea75abd0f58bb8576d0e36b619
1170300304 <cxz2851151@163.com>
2019-09-03 20:36:38 +0800

    Client Base
    Append Client Module

---
+++ Assets/Scripts/Network/ClientBase/LoadProtocolConfigFailedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientBase
+{
+    public class LoadProtocolConfigFailedException : Exception
+    {
+        public LoadProtocolConfigFailedException(string name) : base(name)
+        {
+            //xxxxx
+        }
+    }
+}
