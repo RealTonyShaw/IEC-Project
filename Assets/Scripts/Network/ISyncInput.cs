5d7d6b3144f656a5c31e3a2bea71db4ac6ef30e9
FANTOME <indv.panh@gmail.com>
2019-09-04 20:38:55 +0800

    Add player input synchronization

---
+++ Assets/Scripts/Network/ISyncInput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ISyncPlayerInput
+{
+    /// <summary>
+    /// 同步 Horizontal Axis 的值。
+    /// </summary>
+    /// <param name="instant">发生时刻</param>
+    /// <param name="hAxis">值</param>
+    void SyncHorizontalAxis(long instant, int hAxis);
+    /// <summary>
+    /// 同步 Vertical Axis 的值。
+    /// </summary>
+    /// <param name="instant">发生时刻</param>
+    /// <param name="vAxis">值</param>
+    void SyncVerticalAxis(long instant, int vAxis);
+    /// <summary>
+    /// 同步切换到的技能栏的序号。
+    /// </summary>
+    /// <param name="instant">发生时刻</param>
+    /// <param name="skillIndex">技能栏序号</param>
+    void SyncSwitchSkill(long instant, int skillIndex);
+    /// <summary>
+    /// 同步鼠标左键按下事件。
+    /// </summary>
+    /// <param name="instant">发生时刻</param>
+    void SyncMouseButton0Down(long instant);
+    /// <summary>
+    /// 同步鼠标左键松开事件。
+    /// </summary>
+    /// <param name="instant">发生时刻</param>
+    void SyncMouseButton0Up(long instant);
+    /// <summary>
+    /// 初始化同步类。
+    /// </summary>
+    /// <param name="unit">接受同步的单位</param>
+    void Init(Unit unit);
+}
---
+++ Assets/Scripts/Network/SyncPlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SyncPlayerInput : ISyncPlayerInput
+{
+    Unit unit;
+    public void Init(Unit unit)
+    {
+        this.unit = unit;
+    }
+
+    public void SyncHorizontalAxis(long instant, int hAxis)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SyncMouseButton0Down(long instant)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SyncMouseButton0Up(long instant)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SyncSwitchSkill(long instant, int skillIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SyncVerticalAxis(long instant, int vAxis)
+    {
+        throw new NotImplementedException();
+    }
+}
