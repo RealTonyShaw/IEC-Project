c831654e65cc7039cc7d89e79e672db936765e86
FANTOME <indv.panh@gmail.com>
2019-09-27 20:44:16 +0800

    Add vr package

---
+++ Assets/NVR/NoloVR/Scripts/Editor/NoloVR_BuildEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class NoloVR_BuildEditor {
+
+    //PC平台打包时，将NoloServer打包进Plugins文件夹下面
+    public class MyBuildPostprocessor
+    {
+        [PostProcessBuildAttribute(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string path)
+        {
+#if UNITY_STANDALONE_WIN
+            var strPathFrom = UnityEngine.Application.dataPath + "/Plugins/x86_64/NoloServer.exe";
+            var nIdxSlash = path.LastIndexOf('/');
+            var nIdxDot = path.LastIndexOf('.');
+            var strRootTarget = path.Substring(0, nIdxSlash);
+            var strPathTargetFile = strRootTarget + path.Substring(nIdxSlash, nIdxDot - nIdxSlash) + "_Data/Plugins/NoloServer.exe";
+            System.IO.File.Copy(strPathFrom, strPathTargetFile);
+#endif
+        }
+    }
+}
