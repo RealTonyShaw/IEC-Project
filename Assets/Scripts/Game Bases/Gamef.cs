466cb6bbe056935be248d1e2a5adf111c7fdb6cf
FANTOME <indv.panh@gmail.com>
2019-10-15 23:28:24 +0800

    fix bugs

--- Assets/Scripts/Game Bases/Gamef.cs
+++ Assets/Scripts/Game Bases/Gamef.cs
@@ -10,7 +10,7 @@ public static partial class Gamef
 
     public static long SystemTimeInMillisecond => GameCtrl.IsOnlineGame ? ClientLauncher.Instance.GetTime() : (long)(Time.time * 1000d);
 
-    public static float SystemTime => SystemTimeInMillisecond / 1000f;
+    public static float SystemTime => SystemTimeInMillisecond * 0.001f;
 
     /// <summary>
     /// 对单位造成伤害
